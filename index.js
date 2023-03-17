dc736986e21303fe9b2a51d6220fb1b8f9bc5a81
maksmoroz91 <dlkontakta7@gmail.com>
2023-03-17 14:10:08 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const filmRouter = require('./routers/film.routers');
 const genreRouter = require('./routers/genre.routers');
+const filmGenreRouter = require('./routers/filmGenre.routers');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
@@ -8,5 +9,6 @@ const app = express();
 app.use(express.json());
 app.use('/api', filmRouter);
 app.use('/api', genreRouter);
+app.use('/api', filmGenreRouter);
 
 app.listen(PORT, () => console.log(`Сервер запущен на http://localhost:${PORT}`));
