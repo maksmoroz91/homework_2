459aada3f78a9d39744923d6543e85d74087e8f9
maksmoroz91 <dlkontakta7@gmail.com>
2023-03-17 14:09:32 +0300

    update genre.routers

--- routers/genre.routers.js
+++ routers/genre.routers.js
@@ -3,7 +3,7 @@ const router = new Router();
 const genreController = require('../controller/genre.controller');
 
 router.post('/genre', genreController.createGenre);
-router.get('/genre', genreController.getGenres);
+router.get('/genres', genreController.getGenres);
 router.get('/genre/:id', genreController.getOneGenre);
 router.put('/genre', genreController.updateGenre);
 router.delete('/genre/:id', genreController.deleteGenre);
